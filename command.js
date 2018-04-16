1017357e44966ff3f20be01aeee7657817252861
lukeclee <33207332+lukeclee@users.noreply.github.com>
2018-04-16 13:52:12 -0700

    Add files via upload

--- command.js
+++ command.js
@@ -3,6 +3,10 @@ module.exports = {
 
     fever: function(message) {
         message.channel.send({file: './lul.jpg'});
-    }
+    },
     
+    missus: function(message) {
+        message.channel.send('Oh shoot the missus is here!');
+    }
+
 }
